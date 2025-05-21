0f58ea73d56c6779ef30edba73076b97ba3a2049
Toxex <tonyhallentorp@gmail.com>
2025-05-21 16:40:01 +0200

    failed to launch on local phone via USB

--- components/IngredientsAdder.tsx
+++ components/IngredientsAdder.tsx
@@ -1,2 +1,10 @@
 //something here that controlls the adding of ingredients across malt,
 // hops and yeasts.
+
+//or do i even make the react native thing in here? the inputfield that is.
+
+//or should i jsut do the logic here, which would pretty much just be
+//calling or importing the insert-functions from quries.
+
+//SHOULD ONLY BE THE COMPONENT. LOGIC FOR ADDING STUFF TO
+//THIS INPUTFIELD IN SEPARATE FILE? SERVICE CLASS?
--- database/schema.ts
+++ database/schema.ts
@@ -15,38 +15,29 @@ export async function createSchema() {
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
       alphaAcid REAL,
-      origin TEXT
+      origin TEXT,
+      amountInGrams INTEGER
     );
     CREATE TABLE IF NOT EXISTS yeast (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
       type TEXT,
-      attenuation INTEGER
+      attenuation INTEGER,
+      amountOfPackages INTEGER
     );
     CREATE TABLE IF NOT EXISTS beer_style (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
-      description TEXT
+      description TEXT,
+      FOREIGN KEY (id) REFERENCES malt(id), 
+      FOREIGN KEY (id) REFERENCES hops(id),
+      FOREIGN KEY (id) REFERENCES yeast(id)
     );
     INSERT INTO beer_style (name, description) VALUES ('IPA', 'India Pale Ale is a type of ale that is typically brewed with a high amount of hops.');
     INSERT INTO beer_style (name, description) VALUES ('Lager', 'German lager is a light, crisp, and refreshing beer with a clean, malty aroma and a light, crisp, and refreshing taste.');
     INSERT INTO beer_style (name, description) VALUES ('Stout', 'Dry Stout is a dark, bitter, and full-bodied beer that is typically brewed with a high amount of malt and few hops.');
     INSERT INTO beer_style (name, description) VALUES ('Dark mild', 'Dark mild is a weaker semi-dark ale with caramel notes');
-    CREATE TABLE IF NOT EXISTS style_malt (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      style_id INTEGER NOT NULL,
-      malt_id INTEGER NOT NULL,
-      percentage REAL NOT NULL,
-      FOREIGN KEY (style_id) REFERENCES beer_style(id),
-      FOREIGN KEY (malt_id) REFERENCES malt(id)
-    );
-    CREATE TABLE IF NOT EXISTS style_hops (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      style_id INTEGER NOT NULL,
-      hops_id INTEGER NOT NULL,
-      usage TEXT,
-      FOREIGN KEY (style_id) REFERENCES beer_style(id),
-      FOREIGN KEY (hops_id) REFERENCES hops(id)
-    );
   `);
 }
+// what if beerstyle shoulc contain multiple malts, hops and yeasts?
+//need another table for that i think.
