03ce3fe7bd9c3df1f36d4d02d2a21f386a486c7f
Toxex <tonyhallentorp@gmail.com>
2025-05-25 11:35:51 +0200

    jest added

---
+++ tests/queries.test.ts
@@ -0,0 +1,10 @@
+// import {render} from "@testing-library/react-native";
+// import renderer from "react-test-renderer"
+// import * as queries from "@/database/queries";
+
+// describe("insertMalt", () => {
+//   it("checks if a malt is inserted", () => {
+//     const result = renderer.create(<queries.insertMalt/>).toJSON;
+//     expect
+//   });
+// });
